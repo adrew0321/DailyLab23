d3aa237257132c6082d6c208bb826bcc4c97553a
A'Keem Drew <adrew0321@gmail.com>
2018-08-28 08:08:37 -0400

    init commit

---
+++ LabNumber23/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LabNumber23.Models;
+
+namespace LabNumber23.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult NewUserRegistration()
+        {
+            return View();
+        }
+
+        public ActionResult AddUser(User NewUser)
+        {
+            GC_CoffeeShopEntities ORM = new GC_CoffeeShopEntities();
+
+            // Validation
+
+            // send new user to DB
+            ORM.Users.Add(NewUser);
+
+            // save to DB (Sync)
+            ORM.SaveChanges();
+
+            return RedirectToAction("Index");
+
+           
+        }
+    }
+}
